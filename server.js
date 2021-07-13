30d15a28893aa023387471d58057a70a1334dd73
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2021-07-13 16:49:20 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+const port = 3000;
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+app.listen(port, ()=>{
+    console.loge('Your is up ')
+})
+
+app.get('/getData', (req, res)=>{
+    res.send(projectData)
+})
+
+app.post('/recieveData', (req, res)=>{
+    projectData = req.body
+    /*
+    better way:
+    projectData = {...req.body} speared operator
+    res.end() or res.send
+    */
+})
+/*
+
+*/
---
+++ website/app.js
@@ -0,0 +1,29 @@
+/* Global Variables */
+
+const console = require("console");
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const apiKey 
+
+
+const generate = document.querySelector('#generate');
+
+generate.addEventListener("click", async ()=>{
+    const zipCode = document.querySelector('#zip').value
+
+    if(!zipCode) {
+        alert('please enter your zip code!')
+        return
+    }
+
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`)
+
+    const data = await res.json
+
+
+    const temp = console.logdata.main.temp;
+
+   
+})
